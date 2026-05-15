76dea771abbed6d3ffed315a399794da2605a375
Yuvraj <ys2000126@gmail.com>
2026-05-15 17:58:04 +1200

    Added examples for expected interface for llm and similarity models

---
+++ package/llm/example.py
@@ -0,0 +1,17 @@
+"""
+This is a placeholder to assist development of the high level pipeline. All llm models should align with this.
+"""
+
+def example_llm(commit, issues):
+    """"
+    commit: A commit containing the commit message
+    issues: A list of candidate issues
+    result: A list of issues
+    """
+
+    "TODO: Create the prompt"
+    "TODO: Send the prompt to the llm"
+    "TODO: extract results from the response"
+
+    result = ["issue#1", "issue#2", "issue#3"]
+    return result
---
+++ package/similarity_model/example.py
@@ -0,0 +1,11 @@
+"""
+This is a placeholder to assist development of the high level pipeline. All similarity models should align with this.
+"""
+
+def example_model(commit):
+    """"
+    commit: A commit containing the commit message
+    result: A list of issues
+    """
+    result = ["issue#1", "issue#2", "issue#3"]
+    return result
