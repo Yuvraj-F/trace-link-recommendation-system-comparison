0e4ecc0a17c4808730a52ff3c3b1594c5bfa6731
Yuvraj <ys2000126@gmail.com>
2026-05-15 18:02:24 +1200

    Implemented utils with decompression and unzip methods used to extract the seoss 33 database

---
+++ package/utils.py
@@ -0,0 +1,22 @@
+import bz2
+import shutil
+import zipfile
+
+compressed_file = "db.sqlite3.bz2"
+output_file = "db.sqlite3"
+
+def decompress_bz2(in_path, out_path=None, keep=False):
+    if out_path is None:
+        out_path = in_path.with_suffix("")
+
+    print(f"Decompressing {in_path} into {out_path}...")
+    with bz2.open(in_path, "rb") as src, open(out_path, "wb") as dst:
+        shutil.copyfileobj(src, dst)
+
+    if not keep:
+        in_path.unlink()
+    
+def unzip(in_path, out_path):
+    print(f"Extracting {in_path} into {out_path}...")
+    with zipfile.ZipFile(in_path, "r") as zip_ref:
+        zip_ref.extractall(out_path)
