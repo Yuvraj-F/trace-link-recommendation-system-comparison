c53f4d284901d310603fc77cd99050a4c94e165a
Yuvraj <ys2000126@gmail.com>
2026-05-16 23:13:53 +1200

    Added helper to remove issue id mentions from a commit message
    This is used for preprocessing as issues id in commit messages could potentially assist the models in identifying relevant issues without relying on semantics

--- package/utils.py
+++ package/utils.py
@@ -9,7 +9,7 @@ def decompress_bz2(in_path, out_path=None, keep=False):
     if out_path is None:
         out_path = in_path.with_suffix("")
 
-    print(f"Decompressing {in_path} into {out_path}...")
+    print(f"Decompressing {in_path} into {out_path}")
     with bz2.open(in_path, "rb") as src, open(out_path, "wb") as dst:
         shutil.copyfileobj(src, dst)
 
@@ -17,6 +17,9 @@ def decompress_bz2(in_path, out_path=None, keep=False):
         in_path.unlink()
     
 def unzip(in_path, out_path):
-    print(f"Extracting {in_path} into {out_path}...")
+    print(f"Extracting {in_path} into {out_path}")
     with zipfile.ZipFile(in_path, "r") as zip_ref:
         zip_ref.extractall(out_path)
+
+def remove_issue_id_from_commit(issue_id: str, commit: str):
+    return commit.replace(issue_id, "")
