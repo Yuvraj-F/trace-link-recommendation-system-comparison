5e220ec2120092dfb8301e13ba6a71003d50926a
Yuvraj <ys2000126@gmail.com>
2026-05-20 00:18:43 +1200

    Fixed query

--- package/query.py
+++ package/query.py
@@ -3,4 +3,4 @@ Set of common non-parameterized SQL queries for the SEOSS 33 datatset
 """
 
 SELECT_ALL_TRACE_LINKS = "SELECT * FROM change_set_link join issue on issue.issue_id = change_set_link.issue_id join change_set on change_set.commit_hash = change_set_link.commit_hash;"
-SELECT_ALL_ISSUES = "select * from issues"
+SELECT_ALL_ISSUES = "select * from issue"
