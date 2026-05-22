37e9d18ea7008266a2dc94997d1cafbc51d3bd8a
Yuvraj <ys2000126@gmail.com>
2026-05-22 13:57:41 +1200

    Updated SimModel implementation
    Removed SEOSS33 dependency. Now the caller is responsible for passing in just the documents/issues that serve as the corpus for the retrieval.
    
    Updated SimModel to use the new Issue object
    
    Changed default similarity threshold to 0
    
    The relevant documnets are now sorted by similarityt before being returned. This is important as recall@k metric is measuring how well the true document is identified as being relevant and not sorting the results does not represent the capability of the model accurately.

--- package/similarity_model/model.py
+++ package/similarity_model/model.py
@@ -1,49 +1,45 @@
 import torch
 from sentence_transformers import SentenceTransformer
-from db import SEOSS33
+from transformers import logging
 from query import *
+from db import *
+
+logging.set_verbosity_error()
 
 class SimModel:
     """
-    This is a wrapper class that abstracts away loading models and fetching required data from the database. 
+    This is a wrapper class that abstracts away loading models and pre-computing encodings. 
     NOTE: Current implementation assumes model exists and is publicly available on HuggingFace. If other models
     are needed, this implementation would need to be updated
     """
-    def __init__(self, model, db: SEOSS33):
+    def __init__(self, model, issues: list[Issue]):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model = SentenceTransformer(model, device=self.device)
-        self.db = db
-        self.issues_str, self.issue_embeddings = self._get_issues()
-        
-    def _get_issues(self):
-        issues, keys = self.db.query(SELECT_ALL_ISSUES)   
-        # print(f"Issue keys: {keys}\n")
-        issues_str = []
-        for issue in issues:
-            issues_str.append((issue.summary or "") + ". "  + (issue.description or ""))
-        print("encoding...")
-        embeddings = self.model.encode_document(issues_str)
-        print("done")
-        return issues_str, embeddings
+        self.documnets = issues
+        self.encoded_documents = self.model.encode_document([issue.to_text() for issue in issues])
+
+    def is_cuda(self):
+        return self.device == 'cuda'
     
-    def similarity(self, commit):
+    def batch_encode_queries(self, queries):
+        return self.model.encode_query(queries)
+
+    def get_relevant_issues(self, commit: str, threshold=0) -> list[Issue]:
         """"
         commit: A commit containing the commit message
-        db: Database isntance used to access issues
-
-        result: A list of issues
+        threshold: The minimum required similarity in the range [0, 1]  
+        
+        result: A list of issues sorted by similarity. 
         """
         query = self.model.encode_query(commit)
 
-        similarities = self.model.similarity(query, self.issue_embeddings)[0]
-        # print(len(similarities))
+        similarities = self.model.similarity(query, self.encoded_documents)[0]
+
         result = []
         for i, similarity in enumerate(similarities):
-            if similarity > 0.5:
-                result.append(self.issues_str[i])
+            if similarity >= threshold:
+                result.append((self.documnets[i], similarity))
 
-        # for candidate in result:
-        #     print(candidate[:100] + "...")
-        # print(f"{len(result)} Candidates for query: {commit}")
+        result.sort(key=lambda x: x[1], reverse=True)
 
-        return result
+        return [issue for issue, _ in result]
